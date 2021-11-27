4062e964fd669d96b6fd0494e9e6cbf1197723b0
Allen Cao <allencao@berkeley.edu>
2021-11-26 18:05:12 -0800

    Created naive knapsack dp algorithm

--- solver.py
+++ solver.py
@@ -1,14 +1,44 @@
-from parse import read_input_file, write_output_file
+import math
+import sys
 import os
+from parse import read_input_file, write_output_file
 
 def solve(tasks):
-    """
-    Args:
-        tasks: list[Task], list of igloos to polish
-    Returns:
-        output: list of igloos in order of polishing  
-    """
-    pass
+    def knapSack(W, n, tasks):
+        K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+    
+        # Build table K[][] in bottom up manner
+        for i in range(n + 1):
+            for w in range(W + 1):
+                if i == 0 or w == 0:
+                    K[i][w] = 0
+                elif tasks[i-1].get_duration() <= w:
+                    profit = tasks[i-1].get_late_benefit(w-tasks[i-1].get_deadline())
+                    # profit = arrp[i-1]*math.exp(-0.0170*max(0, w-arrt[i-1]))
+                    do = profit + K[i-1][w-tasks[i-1].get_duration()]
+                    # do = profit + K[i-1][w-arrd[i-1]]
+                    dont = K[i-1][w]
+                    K[i][w] = max(do, dont)
+                else:
+                    K[i][w] = K[i-1][w]
+    
+        maximum = -1
+        for i in range(0, W):
+            maximum = max(maximum, K[n][i])
+        return maximum
+    W = 1440
+    N = len(tasks)
+    return knapSack(W, N, tasks)
+
+
+if __name__ == '__main__':
+    for inp in os.listdir('samples/'):
+        if (inp[-2:] == 'in'):
+            input_path = 'samples/' + inp
+            output_path = 'sample_outputs/' + inp[:-3] + '.out'
+            tasks = read_input_file(input_path)
+            output = solve(tasks)
+            print("profit: " + str(output))
 
 
 # Here's an example of how to run your solver.
