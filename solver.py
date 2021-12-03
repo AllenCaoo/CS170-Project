fec4feebe7f91e142d7c3d0a24d10649dce7225c
Allen Cao <allencao@berkeley.edu>
2021-12-02 18:11:21 -0800

    Implemented history

--- solver.py
+++ solver.py
@@ -3,8 +3,11 @@ from parse import read_input_file, write_output_file
 
 def solve(tasks):
     def knapSack(W, n, tasks):
+        #W = time
+        #n = num tasks
         K = [[0 for x in range(W + 1)] for x in range(n + 1)]
-    
+        history = [[0 for x in range(W + 1)] for x in range(n + 1)]
+
         # Build table K[][] in bottom up manner
         for i in range(n + 1):
             for w in range(W + 1):
@@ -16,41 +19,41 @@ def solve(tasks):
                     do = profit + K[i-1][w-tasks[i-1].get_duration()]
                     # do = profit + K[i-1][w-arrd[i-1]]
                     dont = K[i-1][w]
-                    K[i][w] = max(do, dont)
+                    if do > dont:
+                        K[i][w] = do
+                        history[i][w] = history[i-1][w-tasks[i-1].get_duration()] + 2**(i-1)
+                    else:
+                        K[i][w] = dont
+                        history[i][w] = history[i-1][w]
                 else:
                     K[i][w] = K[i-1][w]
     
         maximum = -1
+        historyMax = 0
         for i in range(0, W):
-            maximum = max(maximum, K[n][i])
-        return maximum
+            if maximum < K[n][i]:
+                maximum = K[n][i]
+                historyMax = history[n][i]
+        return historyMax, maximum
     W = 1440
     N = len(tasks)
     return knapSack(W, N, tasks)
 
 
-# def get_correct(out_path, tasks):
-#     file = open(out_path, 'r')
-#     order = file.readlines()
-#     time = 1
-#     profit = 0
-#     for task in order:
-#         tn = int(task)-1
-#         # print(tasks[tn].get_task_id())
-#         time += tasks[tn].get_duration()
-#         if (time > 1440):
-#             break
-#         profit += tasks[tn].get_late_benefit(time - tasks[tn].get_deadline())
-#     return profit
-
 if __name__ == '__main__':
     for inp in os.listdir('samples/'):
         if (inp[-2:] == 'in'):
             input_path = 'samples/' + inp
-            output_path = 'sample_outputs/' + inp[:-3] + '.out'
+            # output_path = 'sample_outputs/' + inp[:-3] + '.out'
             tasks = read_input_file(input_path)
             actual = solve(tasks)
-            print("actual profit:", str(actual))
+            h = bin(actual[0])
+            i = len(tasks) - 1
+            while i > 0:
+                if h[i] == '1':
+                    print(len(h) - i)
+                i -= 1
+            print(str(actual))
             # expected = get_correct(output_path, tasks)
             # print("expected profit:", str(expected))
 
